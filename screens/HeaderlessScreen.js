5a3ad585f25fd21998a637298cdd22ce75f03fea
hayeon1234 <lhayeon1234@gmail.com>
2022-02-20 15:23:47 +0900

    feat stackNavigator

---
+++ screens/HeaderlessScreen.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {Button, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+function HeaderlessScreen({navigation}) {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Header 없음</Text>
+        <Button onPress={() => navigation.pop()} title="뒤로가기" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default HeaderlessScreen;
